edae04b2686487feecaa858c9ab6d3cc4a17d1b1
cgb2499404424@163.com <cgb2499404424@163.com>
2017-06-11 20:52:35 +0800

    多线程失败版--多线程还是不会啊

--- Chat/src/cn/baidu/com/server/FrameServer.java
+++ Chat/src/cn/baidu/com/server/FrameServer.java
@@ -5,43 +5,87 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 
 public class FrameServer {
 
 	public static void main(String[] args) {
-	
+
+		new FrameServer().start();
+	}
+
+	boolean started = false;
+
+	public void start() {
 		Socket socket = null;
+		ServerSocket ss = null;
 		try {
 			while (true) {
-				ServerSocket ss = new ServerSocket(1314);
-				socket = ss.accept();
-				System.out.println("有一个clean链接:" + socket.getLocalAddress().getHostAddress());
-				OutputStream out = socket.getOutputStream();
-				InputStream in = socket.getInputStream();
-				byte[] b = new byte[1024];
-				int i = 0;
 				try {
-					while ((i = in.read(b)) != -1) {
-						System.out.println(new String(b, 0, i));
-					}
+					ss = new ServerSocket(1314);
+					started = true;
 				} catch (Exception e) {
 					e.printStackTrace();
-					socket.close();// 关闭socket
-					
-					System.out.println("客户端断开链接!");
+					System.out.println("服务器已启动!");
 				}
-
+				socket = ss.accept();
+				System.out.println("有一个clean链接:" + socket.getLocalAddress().getHostAddress());
+				while (started) {
+					Client client = new Client(socket);
+					new Thread(client).start();
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			try {
-				socket.close();// 关闭socket
+				socket.close();
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
 		}
-
 	}
 
+	public class Client implements Runnable {
+
+		private Socket socket;
+		private InputStream in;
+
+		public Client(Socket s) {
+			try {
+				this.socket = s;
+				in = socket.getInputStream();
+				started = false ;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void run() {
+			try {
+				byte[] b = new byte[1024];
+				int i = 0;
+				while ((i = in.read(b)) != -1) {
+					System.out.println(new String(b, 0, i));
+				}
+			} catch (IOException e) {
+				try {
+					socket.close();
+					Thread thread = Thread.currentThread();
+					thread.interrupt();// 终止当前线程
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} finally {
+					try {
+						socket.close();
+						Thread thread = Thread.currentThread();
+						thread.interrupt();// 终止当前线程
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+				e.printStackTrace();
+			}
+		}
+
+	}
 }
