c99e07c2bf3ca4225a53cc582f7b64283c8a497f
cgb2499404424@163.com <cgb2499404424@163.com>
2017-06-11 14:07:50 +0800

    添加输入和显示框去掉中间空白的部分

--- Chat/src/cn/baidu/com/clean/FrameClean.java
+++ Chat/src/cn/baidu/com/clean/FrameClean.java
@@ -1,9 +1,16 @@
 package cn.baidu.com.clean;
 
+import java.awt.BorderLayout;
+import java.awt.TextArea;
+import java.awt.TextField;
+
 public class FrameClean extends java.awt.Frame {
 
 	private static final long serialVersionUID = 1L;
 
+	private TextArea ta = new TextArea();
+	private TextField tf = new TextField();
+	
 	public static void main(String[] args) {
 		
 		new FrameClean().showFrame();
@@ -11,10 +18,14 @@ public class FrameClean extends java.awt.Frame {
 	}
 
 	public void showFrame() {
-		setLocation(400, 300);
-		this.setSize(300, 300);
+		setLocation(450, 300);//相对屏幕位置     左右  , 上下
+		this.setSize(100, 300); 
+		add(tf,BorderLayout.SOUTH);
+		add(ta, BorderLayout.NORTH);
+		pack();//压紧       去除中间的空白部分   
 		setVisible(true);
-
+		
+		
 	}
 
 }
