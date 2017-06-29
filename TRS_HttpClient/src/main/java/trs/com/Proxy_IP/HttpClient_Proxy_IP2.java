e3031a1556c2022db7cac6029b1f32048b256ae6
cgb <cgb2499404424@163.com>
2017-06-29 09:33:09 +0800

    设置链接超时时间/读取超时时间      rjs    2017年6月29日09:33:05

--- TRS_HttpClient/src/main/java/trs/com/Proxy_IP/HttpClient_Proxy_IP2.java
+++ TRS_HttpClient/src/main/java/trs/com/Proxy_IP/HttpClient_Proxy_IP2.java
@@ -12,19 +12,23 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
-public class HttpClient_Proxy_IP {
+public class HttpClient_Proxy_IP2 {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 		HttpGet httpGet = new HttpGet("https://zhidao.baidu.com/question/2202919366028771468.html");
-		//创建代理服务器host
-		HttpHost proxy = new HttpHost("123.118.169.48",8118);
-		httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36");
+		// 创建代理服务器host
+		HttpHost proxy = new HttpHost("123.118.169.48", 8118);
+		httpGet.setHeader("User-Agent",
+				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36");
 		httpGet.setHeader("accept-language", "zh-CN,zh;q=0.8");
-		//请求设置代理
-		RequestConfig config = RequestConfig.custom().setProxy(proxy).build();
-		httpGet.setConfig(config);//设置新添加的代理服务器
+		// 请求设置代理
+		RequestConfig config = RequestConfig.custom().setProxy(proxy)
+				.setConnectTimeout(10000)  //设置链接超时时间
+				.setSocketTimeout(10000)   //设置读取超时时间
+				.build();
+		httpGet.setConfig(config);// 设置新添加的代理服务器
 		CloseableHttpResponse response = httpClient.execute(httpGet);
 		HttpEntity entity = response.getEntity();
 		response.addHeader("accept-language", "zh-CN,zh;q=0.8");
@@ -32,12 +36,9 @@ public class HttpClient_Proxy_IP {
 		String ent = EntityUtils.toString(entity);
 		System.out.println(ent);
 		int code = response.getStatusLine().getStatusCode();
-		
+
 		System.out.println(code);
-		
-		
-		
+
 	}
-	
-	
+
 }
