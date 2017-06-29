7f4c524a7bdd48235332ba42d61c6c655f9463df
cgb <cgb2499404424@163.com>
2017-06-29 08:09:25 +0800

    HttpClient原始方式读取数据写入磁盘   rjs  2017年6月29日08:09:17

--- TRS_HttpClient/src/main/java/trs/com/cn/HttpClient_Demo4.java
+++ TRS_HttpClient/src/main/java/trs/com/cn/HttpClient_Demo4.java
@@ -1,5 +1,6 @@
 package trs.com.cn;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Locale;
@@ -16,6 +17,8 @@ import org.apache.http.util.EntityUtils;
 
 public class HttpClient_Demo4 {
 
+	
+
 	public static void main(String[] args) throws ClientProtocolException, IOException {
 
 		CloseableHttpClient createDefault = HttpClients.createDefault();
@@ -27,14 +30,20 @@ public class HttpClient_Demo4 {
 		// 执行httpGet请求
 		HttpEntity entity = response.getEntity();// 获取实体
 		InputStream in = entity.getContent();
-		
+		String file = "c:/2.txt";
+		byte[] b = new byte[1024];
+		int i = 0 ;
+		FileOutputStream out = new FileOutputStream(file);
+		while ((i=in.read(b))!=-1) {
+			out.write(b, 0, i);
+		}
 		Header contentType = entity.getContentType();// 获取头信息
 		StatusLine statusLine = response.getStatusLine();
 		int code = statusLine.getStatusCode();// 获取干净的状态码
-		System.out.println(code);
+		System.out.println("单独的状态码:  "+code);
 		System.out.println("状态码:  " + statusLine);
 
-		System.out.println(contentType.getValue());   // 返回的类型
+		System.out.println("编码方式: "+contentType.getValue());   // 返回的类型
 		String ent = EntityUtils.toString(entity, "utf-8"); // 把抓取到的页面转换成string形式以UTF-8的形式
 
 		// System.out.println(ent);
