900abfe0619c8bff38c759eed89c3024980696cb
cgb2499404424@163.com <cgb2499404424@163.com>
2017-07-16 08:37:41 +0800

    创建索引   2017年7月16日08:37:31  j

--- Lucene_CGB/src/cn/baidu/CreateIndexTest3.java
+++ Lucene_CGB/src/cn/baidu/CreateIndexTest3.java
@@ -8,6 +8,7 @@ import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.WatchEvent.Kind;
 import java.nio.file.WatchEvent.Modifier;
 import java.nio.file.WatchKey;
@@ -29,19 +30,17 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.NIOFSDirectory;
+import org.apache.lucene.store.RAMDirectory;
 
 import cn.baidu.dao.BookDao;
 import cn.baidu.dao.BookDaoImpl;
 import co.baidu.po.Book;
 
-public class CreateIndexTest2 {
-
-	
-
+public class CreateIndexTest3 {
 
 	/**
 	 * @throws SQLException
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws SQLException, IOException {
 		createIndex();
@@ -54,7 +53,7 @@ public class CreateIndexTest2 {
 		// 2.创建Document文档对象
 		List<Document> documents = new ArrayList<Document>();
 		for (Book book : list) {
-			Document doc = new Document();//document要创建在循环里面                 创建在外面相同的域名不覆盖
+			Document doc = new Document();// document要创建在循环里面 创建在外面相同的域名不覆盖
 			doc.add(new TextField("id", "" + book.getId(), Store.YES));
 			doc.add(new TextField("name", "" + book.getName(), Store.YES));
 			doc.add(new TextField("price", "" + book.getPrice(), Store.YES));
@@ -65,23 +64,21 @@ public class CreateIndexTest2 {
 
 		// 3.创建分析器（分词器）
 		Analyzer a = new StandardAnalyzer();// 创建标准的分词解析器
-	
-		// 创建文件夹对象
-		// 声明索引库位置
-		//Directory d = FSDirectory.open(new File(""));
-		// FSDirectory:硬盘
-		// RAMDirectory:内存
-//		File file = new File("D:/lucene");
-//		Directory directory = FSDirectory.open(file); //声明索引库位置以及索引库的存储方式
+
 		// 4.创建IndexWriterConfig配置信息类
 		IndexWriterConfig conf = new IndexWriterConfig(a);
 		// 5.创建Directory对象，声明索引库存储位置
-		
+
 		Directory directory = NIOFSDirectory.open(FileSystems.getDefault().getPath("e:/lucene"));
-		
+		// 存储到内存中
+		Directory ramDir = new RAMDirectory();
+		Path path = Paths.get("d:/lucene");
+		// 存储到硬盘中
+		FSDirectory fsDir = FSDirectory.open(path);
+
 		// 6.创建IndexWriter写入对象
-		
-		IndexWriter iw = new IndexWriter(directory, conf);
+
+		IndexWriter iw = new IndexWriter(fsDir, conf);
 		// 7.把Document写入到索引库中
 		for (Document docs : documents) {
 			iw.addDocument(docs);
@@ -89,11 +86,9 @@ public class CreateIndexTest2 {
 		// 8.释放资源
 		iw.close();
 		Date d = new Date();
-		long time = d.getTime();
-		System.out.println(time);
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String format = dateFormat.format(d);
-		System.out.println("创建索引完成!"+format);
+		System.out.println("创建索引完成!" + format);
 
 	}
 
